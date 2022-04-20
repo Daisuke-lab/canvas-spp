0760d5e3039f171d0d132c3b98da89fc3a7d16f2
daisuke <daisuke.zr@icloud.com>
2022-04-20 18:25:07 +0900

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import React, { useRef, useEffect, useState } from 'react';
-import useCanvas from '../src/hooks/useCanvas'
-import CustomCanvas from '../src/components/CustomCanvas'
-import dynamic from 'next/dynamic';
-const Conva = dynamic(() => import('../src/components/Conva'), { ssr: false });
-import { useAppSelector, useAppDispatch } from '../src/helpers/hooks'
-//import Conva from "../src/components/Conva"
-import NavBar from "../src/components/nav_bars/NavBar"
-import {TableType,setTables, setConnections} from "../store/reducers/canvasReducer"
-import backendAxios from '../src/helpers/axios';
 
-interface Props {
-  tables: TableType[]
-  connections: ConnectionType[]
-}
+
 const Home: NextPage = (props) => {
-  const {tables, connections} = props as Props
-  const dispatch = useAppDispatch()
-  dispatch(setTables(tables))
-  dispatch(setConnections(connections))
-  return (
-    <div>
-      <NavBar/>
-      <Conva/>
-    </div>
-  )
+    return (
+        <div>
+            <p>hello world</p>
+        </div>)
+
 }
-
-export async function getServerSideProps(context:any) {
-  let tables:TableType[] = []
-  let connections:ConnectionType[] = []
-  try {
-    const res = await backendAxios.get("/api/v1/erDiagram")
-    tables = res.data
-  } catch(err) {
-    console.log(err)
-  }
-
-  try {
-    const res = await backendAxios.get("/api/v1/connection")
-    console.log(res)
-    connections = res.data
-  } catch(err) {
-    console.log(err)
-  }
-
-  return {
-    props: {
-      tables,
-      connections
-    },
-  }
-}
-
-export default Home
